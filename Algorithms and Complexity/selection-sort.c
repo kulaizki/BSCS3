56b7c3de48d3f407c62474f5a6c6b333b19285b5
kulaizki <kulaizke@gmail.com>
2024-09-25 14:22:06 +0800

    feat: add selection sort

---
+++ Algorithms and Complexity/selection-sort.c
@@ -0,0 +1,30 @@
+#include "util.c"
+
+#define SIZE 8
+
+void selectionSort(int a[], int n);
+
+int main() {
+
+    int a[SIZE] = {6, 9, 1, 2, 5, 8, 2, 0};
+
+    selectionSort(a, SIZE);
+    displayArray(a, SIZE);
+}
+
+void selectionSort(int a[], int n) {
+    int x, y;
+    for (x = 0; x < n; ++x) {
+        int min = x;
+        for (y = x + 1; y < n; ++y) {
+            if (a[y] < a[min]) {
+                min = y;
+            }
+        }
+        if (min != x) {
+            int temp = a[x];
+            a[x] = a[min];
+            a[min] = temp;
+        }
+    }
+}
