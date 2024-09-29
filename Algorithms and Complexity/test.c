052fbb1572733b7780cd2b7badfd05a8c7f5e8d5
kulaizki <kulaizke@gmail.com>
2024-09-29 21:47:27 +0800

    feat: add shellsort

--- Algorithms and Complexity/test.c
+++ Algorithms and Complexity/test.c
@@ -12,4 +12,14 @@ int main() {
 
 void sort(int a[], int n) {
     
+    int gap, x, y;
+    for (gap = n / 2; gap > 0; gap /= 2) {
+        for (x = gap; x < n; ++x) {
+            int key = a[x];
+            for (y = x; y >= gap && a[y - gap] > key; y -= gap) {
+                a[y] = a[y - gap];
+            }
+            a[y] = key;
+        }
+    }
 }
