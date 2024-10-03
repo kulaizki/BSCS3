16f551ed3d814925acddbdbc7f6407413aaaa9e3
kulaizki <kulaizke@gmail.com>
2024-10-04 00:31:39 +0800

    feat: add read user data

--- CS-3105/backend-exercise/models/userModel.js
+++ CS-3105/backend-exercise/models/userModel.js
@@ -1,5 +1,16 @@
-const users = [
-  { id: 1, username: 'fitz', password: '12345', email: 'fitz@gmail.com' },
-];
+const fs = require('fs');
+const path = require('path');
+const usersFilePath = path.join(__dirname, '../data/users.json');
 
-module.exports = users;
+// Function to read users from JSON file
+const getUsers = () => {
+  const usersData = fs.readFileSync(usersFilePath);
+  return JSON.parse(usersData);
+};
+
+// Function to write users to JSON file
+const saveUsers = (users) => {
+  fs.writeFileSync(usersFilePath, JSON.stringify(users, null, 2));
+};
+
+module.exports = { getUsers, saveUsers };
