ffabc734169afec9980bc6158ad647d58bd6b766
kulaizki <kulaizke@gmail.com>
2024-10-03 12:58:40 +0800

    refactor: format spacing

--- Algorithms and Complexity/tournament-sort.c
+++ Algorithms and Complexity/tournament-sort.c
@@ -20,20 +20,19 @@ void tournamentSort(int a[], int n){
     int heapN = 2 * n - 1;
     int heap[heapN];
     int startNdx = heapN - 1;
-    int x, y;
-    int P, LC, RC;
+    int x, y, P, LC, RC;
 
-    for(x = n - 1, y = startNdx; x >= 0; --x, --y){
+    for(x = n - 1, y = startNdx; x >= 0; --x, --y) {
         heap[y] = a[x];
     }
 
     for(x = 0; x < n; x++){
-        for(P = (startNdx - 1) / 2; P >= 0; ){
+        for(P = (startNdx - 1) / 2; P >= 0;) {
             LC = 2 * P + 1;
             RC = LC + 1;
 
             LC = (LC < heapN - n) ?  heap[LC] : LC;
-            RC = (RC < heapN- n) ?  heap[RC] : RC;
+            RC = (RC < heapN - n) ?  heap[RC] : RC;
 
             heap[P] = heap[LC] < heap[RC] ? LC : RC;
             P = (x > 0 && P > 0) ? (P - 1) / 2 : P - 1;
