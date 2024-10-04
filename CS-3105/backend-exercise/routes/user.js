c4e5eb07612de31ef9bd2c0f6c89ca1974f44535
kulaizki <kulaizke@gmail.com>
2024-10-04 22:41:41 +0800

    feat: add rate limiter to profile

--- CS-3105/backend-exercise/routes/user.js
+++ CS-3105/backend-exercise/routes/user.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const { register, login, profile } = require('../controllers/userController');
-const authMiddleware = require('../middleware/authMiddleware');
+const { authMiddleware, limiter } = require('../middleware/authMiddleware'); 
 
 router.post('/register', register);
 router.post('/login', login);
-router.get('/profile', authMiddleware, profile);
+router.get('/profile', limiter, authMiddleware, profile); 
 
 module.exports = router;
