3a70dd6aeca6583086c968ede2a77d67c4f71ab9
kulaizki <kulaizke@gmail.com>
2024-10-04 22:42:52 +0800

    feat: add error handling

--- CS-3105/backend-exercise/models/userModel.js
+++ CS-3105/backend-exercise/models/userModel.js
@@ -2,15 +2,24 @@ const fs = require('fs');
 const path = require('path');
 const usersFilePath = path.join(__dirname, '../data/users.json');
 
-// Function to read users from JSON file
+// read users from JSON file
 const getUsers = () => {
-  const usersData = fs.readFileSync(usersFilePath);
-  return JSON.parse(usersData);
+  try {
+    const usersData = fs.readFileSync(usersFilePath, 'utf8');
+    return JSON.parse(usersData);
+  } catch (error) {
+    console.error('Error reading users from file:', error);
+    return []; // Return an empty array if there's an error
+  }
 };
 
-// Function to write users to JSON file
+// write users to JSON file
 const saveUsers = (users) => {
-  fs.writeFileSync(usersFilePath, JSON.stringify(users, null, 2));
+  try {
+    fs.writeFileSync(usersFilePath, JSON.stringify(users, null, 2), 'utf8');
+  } catch (error) {
+    console.error('Error writing users to file:', error);
+  }
 };
 
 module.exports = { getUsers, saveUsers };
