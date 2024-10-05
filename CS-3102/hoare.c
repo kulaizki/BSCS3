4f0a02bcc5ddb932aa6a130d30299906cd819e7d
kulaizki <kulaizke@gmail.com>
2024-10-05 10:45:55 +0800

    feat: add quicksort hoare implementation

---
+++ CS-3102/hoare.c
@@ -0,0 +1,33 @@
+#include "util.c"
+
+void quickSort(int a[], int l, int h);
+
+int main() {
+
+    int a[SIZE] = {6, 9, 1, 2, 5, 8, 2, 0};
+
+    quickSort(a, 0, SIZE - 1);
+    displayArray(a, SIZE);
+}
+
+void quickSort(int a[], int l, int h) {
+    if (l < h) {
+        int pivot = a[l];  
+        int x = l - 1, y = h + 1;
+
+        while (1) {
+            for (x++; a[x] < pivot; x++) {}
+            for (y--; a[y] > pivot; y--) {}
+
+            if (x >= y) break;
+
+            int temp = a[x];
+            a[x] = a[y];
+            a[y] = temp;
+        }
+
+        quickSort(a, l, y);     
+        quickSort(a, y + 1, h); 
+    }
+}
+
