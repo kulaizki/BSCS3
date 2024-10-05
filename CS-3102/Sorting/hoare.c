f95eac56090aeb58bb459adc8c49066ccdc0ceaf
kulaizki <kulaizke@gmail.com>
2024-10-05 14:09:10 +0800

    refactor: remove partition visualization

--- CS-3102/Sorting/hoare.c
+++ CS-3102/Sorting/hoare.c
@@ -1,39 +1,19 @@
 #include "util.c"
-#include <stdio.h>
 
 void quickSort(int a[], int l, int h);
 
-// Utility function to visualize the array at each step
-void displayArrayWithMarkers(int a[], int size, int l, int h, int pivotIndex) {
-    for (int i = 0; i < size; i++) {
-        if (i == pivotIndex)
-            printf("[%d]* ", a[i]);  // Mark pivot
-        else if (i == l)
-            printf("[%d]L ", a[i]);  // Mark left boundary
-        else if (i == h)
-            printf("[%d]H ", a[i]);  // Mark right boundary
-        else
-            printf("%d ", a[i]);
-    }
-    printf("\n");
-}
-
 int main() {
+
     int a[SIZE] = {6, 9, 1, 2, 5, 8, 2, 0};
 
-    printf("Initial array:\n");
-    displayArray(a, SIZE);
     quickSort(a, 0, SIZE - 1);
-    printf("Sorted array:\n");
-    displayArray(a, SIZE);
+    displayArray(a, SIZE);  
 }
 
 void quickSort(int a[], int l, int h) {
     if (l < h) {
         int pivot = a[l];
         int x = l - 1, y = h + 1;
-
-        printf("\nPartitioning with pivot %d (index %d):\n", pivot, l);
 
         while (1) {
             for (x++; a[x] < pivot; x++) {}
@@ -44,15 +24,9 @@ void quickSort(int a[], int l, int h) {
             int temp = a[x];
             a[x] = a[y];
             a[y] = temp;
-
-            printf("After swapping a[%d] and a[%d]:\n", x, y);
-            displayArray(a, SIZE);  
         }
 
-        printf("Partition complete:\n");
-        displayArrayWithMarkers(a, SIZE, l, h, y);
-
-        quickSort(a, l, y);  // Sort left partition
-        quickSort(a, y + 1, h);  // Sort right partition
+        quickSort(a, l, y);     // Sort left partition
+        quickSort(a, y + 1, h); // Sort right partition
     }
 }
