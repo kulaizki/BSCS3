d5b424debb38084047b04b8a2b717dc13fb9cf48
kulaizki <kulaizke@gmail.com>
2024-10-07 01:02:14 +0800

    refactor: optimize bucket sort with linked list

--- CS-3102/Sorting/bucket-sort.c
+++ CS-3102/Sorting/bucket-sort.c
@@ -1,49 +1,81 @@
 #include "util.c"
+#include <stdlib.h>
+
+typedef struct Node {
+    int data;
+    struct Node *link;
+} *LIST;
 
 void bucketSort(float a[], int n);
+void insertSorted(LIST *bucket, int value);
+void freeBuckets(LIST bucket[], int n);
 
-int main() {
-
+int main()
+{
     float a[SIZE] = {0.78, 0.17, 0.39, 0.26, 0.72, 0.94, 0.21, 0.12};
     
     bucketSort(a, SIZE);
-    displayArrayF(a, SIZE);  
+    displayArrayF(a, SIZE);
 }
 
-void bucketSort(float a[], int n) {
-    // Create n empty buckets
-    float b[SIZE][SIZE];   // Buckets
-    int bN[SIZE] = {0};    // Bucket sizes
+void bucketSort(float a[], int n)
+{
+    // Create an array of linked lists (buckets)
+    LIST buckets[SIZE] = {NULL};
 
-    // Put elements into different buckets
-    for (int x = 0; x < n; ++x) {
-        int bIdx = n * a[x];  // Calculate bucket index
-        b[bIdx][bN[bIdx]++] = a[x];  // Add to bucket and increment bucket size
+    // Find the bucket index and insert elements into their respective buckets
+    for (int x = 0; x < n; ++x)
+    {
+        int bIdx = n * a[x];  // Calculate bucket index (assuming values between 0 and 1)
+        insertSorted(&buckets[bIdx], (int)(a[x] * 100));  // Insert into the appropriate bucket after scaling
     }
 
-    // Sort individual buckets using insertion sort
-    for (int x = 0; x < n; x++) {
-        int subSeqN = bN[x];  // Number of elements in current bucket
-        float* subSeq = b[x]; // Current bucket 
-
-        // Insertion sort for individual bucket 
-        for (int y = 1; y < subSeqN; ++y) {
-            float key = subSeq[y];
-            int z = y - 1;
-
-            while (z >= 0 && subSeq[z] > key) {
-                subSeq[z + 1] = subSeq[z];
-                z--;
-            }
-            subSeq[z + 1] = key;
+    // Concatenate all the sorted buckets back into the array
+    int z = 0;
+    for (int x = 0; x < n; ++x)
+    {
+        LIST node = buckets[x];
+        while (node != NULL)
+        {
+            a[z++] = node->data / 100.0;  // Copy sorted elements back to array and scale down
+            node = node->link;
         }
     }
 
-    // Concatenate all buckets into result array
-    int z = 0;  // Index for original array
-    for (int x = 0; x < n; ++x) {
-        for (int y = 0; y < bN[x]; ++y, ++z) {
-            a[z++] = b[x][y];  // Copy sorted elements back to a[]
+    freeBuckets(buckets, n);
+}
+
+// Insert a value into the bucket (linked list) in sorted order
+void insertSorted(LIST *bucket, int value)
+{
+    LIST newNode = (LIST)malloc(sizeof(struct Node));
+    newNode->data = value;
+    newNode->link = NULL;
+
+    // If the bucket is empty or the new value is smaller than the head
+    if (*bucket == NULL || (*bucket)->data >= value) {
+        newNode->link = *bucket;
+        *bucket = newNode;
+    } else {
+        LIST current = *bucket;
+        while (current->link != NULL && current->link->data < value) {
+            current = current->link;
+        }
+        newNode->link = current->link;
+        current->link = newNode;
+    }
+}
+
+void freeBuckets(LIST bucket[], int n)
+{
+    for (int i = 0; i < n; ++i)
+    {
+        LIST node = bucket[i];
+        while (node != NULL)
+        {
+            LIST temp = node;
+            node = node->link;
+            free(temp);
         }
     }
 }
