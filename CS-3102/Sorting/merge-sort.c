1bd17ff878fa50a05b3e851375e7e33316241847
kulaizki <kulaizke@gmail.com>
2024-10-06 21:37:20 +0800

    feat: reformat code

--- CS-3102/Sorting/merge-sort.c
+++ CS-3102/Sorting/merge-sort.c
@@ -15,16 +15,14 @@ void merge(int a[], int l, int m, int r) {
     int n1 = m - l + 1;
     int n2 = r - m;
 
-    // Create temp arrays
-    int L[n1], R[n2];
+    int *L = (int *)malloc(n1 * sizeof(int));
+    int *R = (int *)malloc(n2 * sizeof(int));
 
     // Copy data to temp arrays
-    for (int x = 0; x < n1; x++) {
+    for (int x = 0; x < n1; x++)
         L[x] = a[l + x];
-    }
-    for (int y = 0; y < n2; y++) {
+    for (int y = 0; y < n2; y++)
         R[y] = a[m + 1 + y];
-    }
 
     // Merge the temp arrays back into a[]
     int x = 0, y = 0, z;
@@ -47,6 +45,9 @@ void merge(int a[], int l, int m, int r) {
     for (; y < n2; y++, z++) {
         a[z] = R[y];
     }
+
+    free(L);
+    free(R);
 }
 
 void mergeSort(int a[], int l, int r) {
--- CS-3102/Sorting/test.c
+++ CS-3102/Sorting/test.c
@@ -10,27 +10,28 @@ int main() {
     int a[SIZE] = {4, 1, 2, 3, 9, 9, 0, 3};
     
     quickSort(a, 0, SIZE - 1);
+    // sort(a, SIZE);
     displayArray(a, SIZE);
 }
 
 void quickSort(int a[], int l, int h) {
     if (l < h) {
-        int p = a[l];
+        int p = a[h];
         int x = l - 1;
-        int y = h + 1;
-
-        for (;;) {
-            do { ++x; } while (a[x] < p);
-            do { --y; } while (a[y] > p);
-
-            if (x >= y) break;
-
-            int temp = a[x];
-            a[x] = a[y];
-            a[y] = temp;
+        for (int y = l; y < h; ++y) {
+            if (a[y] <= p) {
+                ++x;
+                int temp = a[x];
+                a[x] = a[y];
+                a[y] = temp;
+            }
         }
 
-        quickSort(a, l, y);
-        quickSort(a, y + 1, h);
+        int temp = a[x + 1];
+        a[x + 1] = a[h];
+        a[h] = temp;
+
+        quickSort(a, l, x);
+        quickSort(a, x + 2, h);
     }
 }
