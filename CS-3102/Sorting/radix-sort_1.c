7307aa433e82e1f5c86b318e4ccdf26c68acc348
kulaizki <kulaizke@gmail.com>
2024-10-07 14:57:53 +0800

    refactor: summarize comments

--- CS-3102/Sorting/radix-sort_1.c
+++ CS-3102/Sorting/radix-sort_1.c
@@ -16,23 +16,22 @@ void countingSort(int a[], int n, int exp) {
     int res[n]; // sorted
     int freq[10] = {0}; // digit count 
 
-    // Store the count of occurrences of each digit
+    // count of occurrences of each digit
     for (int x = 0; x < n; x++) {
         freq[(a[x] / exp) % 10]++;
     }
 
-    // Change count so that it contains the actual position of the digit in res[]
+    // change count so that it contains the actual position of the digit in res[]
     for (int x = 1; x < 10; x++) {
         freq[x] += freq[x - 1];
     }
 
-    // Build the sorted array
+    // build the sorted array
     for (int x = n - 1; x >= 0; x--) {
         res[freq[(a[x] / exp) % 10] - 1] = a[x];
         freq[(a[x] / exp) % 10]--;
     }
 
-    // Copy the sorted array to a[]
     for (int x = 0; x < n; x++) {
         a[x] = res[x];
     }
